47018992388587c0b74809cfc408da4883b678da
nishanth kumar <nishanth.kumarm117@gmail.com>
2024-03-12 13:43:20 -0400

    update

--- main.tf
+++ main.tf
@@ -36,19 +36,3 @@ resource "aws_s3_bucket_object" "error_file" {
   source = "docs/build/html/error.html"
   content_type = "text/html"
 }
-
-resource "aws_s3_bucket_policy" "my_bucket_policy" {
-  bucket = aws_s3_bucket.my_bucket.bucket
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = "*",
-        Action = "s3:GetObject",
-        Resource = "${aws_s3_bucket.my_bucket.arn}/*"
-      },
-    ],
-  })
-}
